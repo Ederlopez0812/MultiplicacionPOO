fe60644a5203a0ea94a9431888b517f4354f671b
Eder Lopez <ederlopez8820@gmail.com>
2024-04-25 18:49:31 -0600

    divisionp1

---
+++ MainMultiplicacion.java
@@ -0,0 +1,28 @@
+
+// se importa la libreria Scanner
+import java.util.Scanner;
+
+// se declara la clase
+public class MainMultiplicacion {
+    // se agrega el metodo main
+    public static void main(String[] args) {
+        // se llama al metodo Scanner
+        Scanner n1 = new Scanner(System.in);
+        // se ingresa el mensaje que se le dara al usuario
+
+        System.out.println("Ingresa un numero");
+        // se almacena el valor del usuario
+        int a = n1.nextInt();
+        // se almacena el valor del usuario
+        System.out.println("Ingrese el segundo numero");
+        // se almacena el valor del usuario
+        int b = n1.nextInt();
+        // se hace la operacion con los datos ingresados por el usuario
+        int r = a * b;
+        // se imprime el resultado de la operacion y se muestra en pantalla
+        System.out.println("El resultado es:" + r);
+
+        // se cierra el metodo mian
+    }
+    // se cierra clase
+}
